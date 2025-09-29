c63e9c50f78e803a1f0f38a05d2cd5271cdf0f7e
DanieleGibilaro <41300808+DanieleGibilaro@users.noreply.github.com>
2025-09-29 09:56:29 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1161,11 +1161,12 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle(QStyleFactory.create("Fusion"))
     
-    # Applica stile iniziale (light mode) lo stile rimane non completamente modificabile (ho sbagliato a buttare il codice in un software per aid programming :( rimane nel todo)
+    # non viene piu realmente caricato il light poiché attualmente lo stile é bloccato a dark perché dopo test é stato scoperto che veniva scelto e usato solo lui , é anche piu bellino
     settings = SettingsTab()
     settings.apply_light_style()
     
     monitor = BluecarMonitor()
     monitor.show()
     sys.exit(app.exec_())
+
 
